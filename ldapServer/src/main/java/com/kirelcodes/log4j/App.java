f7f6d9be4cf1cc60e5b4387cca967fb19549f3a7
Barr Kirel <barr.kirel.41@gmail.com>
2021-12-26 15:42:51 +0200

    Added error message for not enough arguments

--- ldapServer/src/main/java/com/kirelcodes/log4j/App.java
+++ ldapServer/src/main/java/com/kirelcodes/log4j/App.java
@@ -2,7 +2,12 @@ package com.kirelcodes.log4j;
 
 public class App {
 	public static void main(String args[]) throws Exception{
-		System.out.println(String.format("Listening at: ldap://%s:%d and codebase is: ", args[0], Integer.parseInt( args[1]),
+		if(args.length != 3){
+			System.out.println("You must provide a <hostname> <port> and <codebase>");
+			return;
+		}
+		System.out.println(String.format("Listening at: ldap://%s:%d and codebase is: %s", args[0],
+										 Integer.parseInt( args[1]),
 										 args[2]));
 		Server.run(args[0], Integer.parseInt(args[1]), args[2]);
 	}
